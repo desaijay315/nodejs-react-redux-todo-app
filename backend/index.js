d9ef653988458f96d1a2548fe8813994d8361016
desaijay315 <desaijay315@gmail.com>
2021-02-16 18:21:06 +0530

    frontend path correction

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,11 @@ console.log(path.join(__dirname, '../frontend/build'))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html')))
 } else {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html')))
 }
 
 
