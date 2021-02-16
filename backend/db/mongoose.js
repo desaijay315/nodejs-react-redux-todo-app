66a86ab3dfb541c39e4ad68267d9f18f018eed75
desaijay315 <desaijay315@gmail.com>
2021-02-16 16:03:26 +0530

    mongoose uri

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://desai321:desai321@cluster0.bg48i.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
